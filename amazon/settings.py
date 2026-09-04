6b253039005e7a563e385facb412cb9867aff897
Shiva Prasad <shivaderangula90@gmail.com>
2026-09-04 17:35:08 +0530

    original allowed

--- amazon/settings.py
+++ amazon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8)qeurakg39vu9pnk7_gs=thn&ih)b*yy-7h9un(+vu$6emg$_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.render.com']
 
 
 # Application definition
