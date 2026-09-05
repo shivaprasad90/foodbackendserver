3624d85c527c10c59232aa6d024d404b6e7dcbe3
Shiva Prasad <shivaderangula90@gmail.com>
2026-09-05 15:15:22 +0530

    allowed

--- amazon/settings.py
+++ amazon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8)qeurakg39vu9pnk7_gs=thn&ih)b*yy-7h9un(+vu$6emg$_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com','https://react-first-project-murex.vercel.app/']
 
 
 # Application definition
